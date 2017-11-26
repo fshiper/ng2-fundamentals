a76813f7d4069f01db76848c0737dd7080fab713
fshiper <przemyslaw.krzywodajc@gmail.com>
2017-11-26 23:29:28 +0100

    sunday

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'events-app',
-    template: '<h2><events-list></events-list></h2>'
+    template: '<events-list></events-list>'
 })
 
 export class EventsAppComponent {
