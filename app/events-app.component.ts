a6472447783a48e94ccfb96046c652ccdc68ddfd
Fshiper <przemyslaw.krzywodajc@gmail.com>
2017-12-13 16:45:09 +0100

    Services

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core'
 
 @Component({
-    selector: 'events-app',
-    template: `
+  selector: 'events-app',
+  template: `
     <nav-bar></nav-bar>
     <events-list></events-list>
-    `
+  `
 })
-
 export class EventsAppComponent {
 
 }
